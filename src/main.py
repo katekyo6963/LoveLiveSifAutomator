2996a26b3cefb7f09abfaa13cd150602831956a7
kawaken1025 <aibasisyou090@gmail.com>
2019-06-07 22:01:50 +0900

    なんかいろいろ追加

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+=========================================
+ comment     : None
+ param       : None
+ return      : None
+========================================
+ Display Dpl : BlueStacks ???
+=========================================
+ Copy Right (C) 2019 All Right Reserved.
+   @author LL_SIF_AT @kawaken1025
+     Create Date : 2019/06/27
+=========================================
+"""
