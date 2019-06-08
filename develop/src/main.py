99334fcd25044e96ec4ec10cd1631a66521cb1c9
kawaken1025 <aibasisyou090@gmail.com>
2019-06-08 09:52:06 +0900

    譜面データ周りの説明追加

--- develop/src/main.py
+++ develop/src/main.py
@@ -11,3 +11,13 @@
      Create Date : 2019/06/27
 =========================================
 """
+
+import time
+
+def live_operator(fumen, slp):
+    for i in range(len(fumen)):
+        if fumen[i] != "-":
+            # btn tap
+            time.sleep(1)
+        else:
+            time.sleep(slp)
